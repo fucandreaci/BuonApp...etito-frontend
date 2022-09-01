4dc79abaeb51aa2a855038d80f849f964b39beb1
Andrea Fucci <afucci@soldo.com>
2022-09-02 00:08:24 +0200

    Setup i18n

--- src/App.tsx
+++ src/App.tsx
@@ -1,57 +1,87 @@
-import React from 'react';
+import React, {Suspense} from 'react';
+import itTexts from 'i18n/common-translations/it.json';
+import enTexts from 'i18n/common-translations/en.json';
+import esTexts from 'i18n/common-translations/es.json';
+import frTexts from 'i18n/common-translations/fr.json';
+import deTexts from 'i18n/common-translations/de.json';
+import nlTexts from 'i18n/common-translations/nl.json';
+import svTexts from 'i18n/common-translations/sv.json';
 import logo from './logo.svg';
 import { Counter } from './features/counter/Counter';
+import { loadTranslations} from 'i18n/i18n.service';
+
+
 import './App.css';
+import {useTranslation} from 'react-i18next';
 
 function App() {
+  const i18nTexts = {
+    'en': enTexts,
+    'it': itTexts,
+    'de': deTexts,
+    'es': esTexts,
+    'fr': frTexts,
+    'nl': nlTexts,
+    'sv': svTexts,
+  };
+  loadTranslations('commons', i18nTexts);
+
+  const {t} = useTranslation('commons');
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <Counter />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <span>
-          <span>Learn </span>
-          <a
-            className="App-link"
-            href="https://reactjs.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux-toolkit.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux Toolkit
-          </a>
-          ,<span> and </span>
-          <a
-            className="App-link"
-            href="https://react-redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React Redux
-          </a>
-        </span>
-      </header>
-    </div>
+      <><Suspense fallback="loading">
+        <div className="App">
+          <header className="App-header">
+            <img src={logo} className="App-logo" alt="logo"/>
+            <Counter/>
+            <p>
+              <>{t('common:BACK')}
+                <br/>
+                Edit <code>src/App.tsx</code> and save to reload.
+              </>
+            </p>
+            <span>
+                      <span>Learn </span>
+                      <a
+                          className="App-link"
+                          href="https://reactjs.org/"
+                          target="_blank"
+                          rel="noopener noreferrer"
+                      >
+                          React
+                      </a>
+                      <span>, </span>
+                      <a
+                          className="App-link"
+                          href="https://redux.js.org/"
+                          target="_blank"
+                          rel="noopener noreferrer"
+                      >
+                          Redux
+                      </a>
+                      <span>, </span>
+                      <a
+                          className="App-link"
+                          href="https://redux-toolkit.js.org/"
+                          target="_blank"
+                          rel="noopener noreferrer"
+                      >
+                          Redux Toolkit
+                      </a>
+                      ,<span> and </span>
+                      <a
+                          className="App-link"
+                          href="https://react-redux.js.org/"
+                          target="_blank"
+                          rel="noopener noreferrer"
+                      >
+                          React Redux
+                      </a>
+                  </span>
+          </header>
+        </div>
+          </Suspense>
+      </>
   );
 }
 
--- src/index.tsx
+++ src/index.tsx
@@ -5,15 +5,20 @@ import { store } from './app/store';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import './index.css';
+import {i18Mfe, initAppTranslator} from './i18n/i18n.config';
+import {I18nextProvider} from 'react-i18next';
 
 const container = document.getElementById('root')!;
 const root = createRoot(container);
 
+initAppTranslator();
+
 root.render(
   <React.StrictMode>
     <Provider store={store}>
-      <App />
-    </Provider>
+      <I18nextProvider i18n={ i18Mfe }><App /></I18nextProvider>,
+
+      </Provider>
   </React.StrictMode>
 );
 
