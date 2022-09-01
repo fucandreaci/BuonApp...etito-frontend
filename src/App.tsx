9b30207e8c6dbc9e222b523979b4baa60fca7d5d
Andrea Fucci <afucci@soldo.com>
2022-09-02 00:28:56 +0200

    Implemented base route

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, {Suspense} from 'react';
+import React from 'react';
 import itTexts from 'i18n/common-translations/it.json';
 import enTexts from 'i18n/common-translations/en.json';
 import esTexts from 'i18n/common-translations/es.json';
@@ -6,78 +6,38 @@ import frTexts from 'i18n/common-translations/fr.json';
 import deTexts from 'i18n/common-translations/de.json';
 import nlTexts from 'i18n/common-translations/nl.json';
 import svTexts from 'i18n/common-translations/sv.json';
-import logo from './logo.svg';
-import { loadTranslations} from 'i18n/i18n.service';
+import {Route, Router, Switch} from 'react-router-dom';
+import {loadTranslations} from 'i18n/i18n.service';
+import { createBrowserHistory } from "history";
 
 
 import './App.css';
 import {useTranslation} from 'react-i18next';
 
 function App() {
-  const i18nTexts = {
-    'en': enTexts,
-    'it': itTexts,
-    'de': deTexts,
-    'es': esTexts,
-    'fr': frTexts,
-    'nl': nlTexts,
-    'sv': svTexts,
-  };
-  loadTranslations('commons', i18nTexts);
+    const i18nTexts = {
+        'en': enTexts,
+        'it': itTexts,
+        'de': deTexts,
+        'es': esTexts,
+        'fr': frTexts,
+        'nl': nlTexts,
+        'sv': svTexts,
+    };
+    loadTranslations('commons', i18nTexts);
 
-  const {t} = useTranslation('commons');
+    const {t} = useTranslation('commons');
+    const customHistory = createBrowserHistory();
 
-  return (
-        <div className="App">
-          <header className="App-header">
-            <img src={logo} className="App-logo" alt="logo"/>
-            <p>
-              <>{t('common:BACK')}
-                <br/>
-                Edit <code>src/App.tsx</code> and save to reload.
-              </>
-            </p>
-            <span>
-                      <span>Learn </span>
-                      <a
-                          className="App-link"
-                          href="https://reactjs.org/"
-                          target="_blank"
-                          rel="noopener noreferrer"
-                      >
-                          React
-                      </a>
-                      <span>, </span>
-                      <a
-                          className="App-link"
-                          href="https://redux.js.org/"
-                          target="_blank"
-                          rel="noopener noreferrer"
-                      >
-                          Redux
-                      </a>
-                      <span>, </span>
-                      <a
-                          className="App-link"
-                          href="https://redux-toolkit.js.org/"
-                          target="_blank"
-                          rel="noopener noreferrer"
-                      >
-                          Redux Toolkit
-                      </a>
-                      ,<span> and </span>
-                      <a
-                          className="App-link"
-                          href="https://react-redux.js.org/"
-                          target="_blank"
-                          rel="noopener noreferrer"
-                      >
-                          React Redux
-                      </a>
-                  </span>
-          </header>
-        </div>
-  );
+    return (
+        <Switch>
+            <Router history={customHistory}>
+                <Route path="/login" exact={true}>
+                    HOME
+                </Route>
+            </Router>
+        </Switch>
+    );
 }
 
 export default App;
